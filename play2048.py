2825f28996a6de64778f932e1bae393a43b5aa65
Richard Tran <rtran4@mail.valenciacollege.edu>
2016-04-22 00:26:15 -0400

    2048 final

--- play2048.py
+++ play2048.py
@@ -4,6 +4,20 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 def play2048( times ):
+    browser = webdriver.Firefox()
+    browser.get('https://gabrielecirculli.github.io/2048/')
+    htmlElem = browser.find_element_by_tag_name('html')
+    FinalScoreElem = browser.find_element_by_class('score-container')
+    moves = 0
+    for moves in range(times):
+        htmlElem.send_keys(Keys.UP)
+        htmlElem.send_keys(Keys.RIGHT)
+        htmlElem.send_keys(Keys.DOWN)
+        htmlElem.send_keys(Keys.LEFT)
+        moves += 1
+
+    print('Final score = '+FinalScoreElem.text)
+
     #TODO: write code in this function that:
     # 1. opens a game of 2048 from the URL: https://gabrielecirulli.github.io/2048/
     # 2. uses the parameter 'times' to determine how many times your code will try to play the game
